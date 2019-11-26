a0b707bf083a668788561f1282a6d33267de8ec1
Márcio Flávio <mflaviof1995@gmail.com>
2019-11-26 12:11:00 -0300

    second class

--- main.go
+++ main.go
@@ -9,10 +9,28 @@ type contaCorrente struct {
 	saldo    float64
 }
 
+func (c *contaCorrente) sacar(saque float64) string {
+
+	podeSacar := c.saldo > saque
+	naoNegativo := saque > 0
+
+	if podeSacar && naoNegativo {
+
+		c.saldo -= saque
+
+		return "Saque realizado com sucesso"
+
+	} else {
+
+		return "Por algum motivo não foi possível realizar o saque"
+	}
+
+}
+
 func main() {
 
 	contaAlfredo := contaCorrente{"Alfredo", 154, 666, 5.24}
 
-	fmt.Println(contaAlfredo)
+	fmt.Printf("%s, %.2f \n", contaAlfredo.sacar(6), contaAlfredo.saldo)
 
 }
